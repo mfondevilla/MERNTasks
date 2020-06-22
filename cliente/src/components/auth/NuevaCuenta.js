2b9b6273227ec63dc1373ad24eaf9de095fb65db
Melania <m_fondevilla@hotmail.com>
2020-06-22 11:17:03 +0200

    load cliente folder

---
+++ cliente/src/components/auth/NuevaCuenta.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NuevaCuenta = () => {
+    return (  
+        <h1>desde nueva cuenta</h1>
+    );
+}
+ 
+export default NuevaCuenta;
---
+++ cliente/src/components/proyectos/Proyectos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Proyectos = () => {
+    return ( 
+        <h1>desde proyectos</h1>
+     );
+}
+ 
+export default Proyectos;
